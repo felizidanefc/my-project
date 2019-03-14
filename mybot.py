e9d1b561dd4e60518c74cacb18a36a12a1acc3fe
felizidanefc <48539050+felizidanefc@users.noreply.github.com>
2019-03-14 12:02:44 +0100

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -4,10 +4,10 @@ bot = telebot.TeleBot("635333939:AAHPDBHRhzELzbRBsrG6PfeXHUiXGIZm1xc")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Bienvenido shur, al BOT de RotoCoches")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
-
+	bot.reply_to(message, "No te entiendo")
+print("El bot se ha ejecutado correctamente")
 bot.polling()
